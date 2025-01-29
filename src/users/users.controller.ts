42dd2edb596ca35ca7d110e93c003214cc993a39
anushkaNirmal <niranushka1@gmail.com>
2025-01-29 09:21:45 +0530

    providers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,31 +7,37 @@ import {
   Post,
   Delete,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   @Get() //GET /users
   findAll() {
-    return [];
+    return this.userService.findAll();
   }
 
   @Get(':id') //GET /users:id
   findById(@Param('id') id: string) {
-    return { id };
+    return this.userService.findById(+id); //unary plus convert string to number
   }
 
   @Post() //POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(@Body() user: { name: string; email: string }) {
+    return this.userService.create(user);
   }
 
   @Patch(':id') //PATCH /users/:id
-  findOneAndUpdate(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  findOneAndUpdate(
+    @Param('id') id: string,
+    @Body() userUpdate: { name?: string; email?: string },
+  ) {
+    return this.userService.update(+id, userUpdate);
   }
 
   @Delete(':id') //DELETE /users:id
   findByIdAndDelete(@Param('id') id: string) {
-    return { id };
+    return this.userService.delete(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'Anushka Nirmal', email: 'abc@def.com' },
+    { id: 2, name: 'John Doe', email: 'abc@def.com' },
+    { id: 3, name: 'Jane Dane', email: 'abc@def.com' },
+    { id: 4, name: 'Mike Ross', email: 'abc@def.com' },
+    { id: 5, name: 'Rachel Zane', email: 'abc@def.com' },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  create(user: { name: string; email: string }) {
+    const newId = [...this.users].sort((a, b) => b.id - a.id)[0].id + 1;
+    const newUser = { id: newId, ...user };
+    this.users.push(newUser);
+    return this.findById(newId);
+  }
+
+  update(id: number, userUpdate: { name?: string; email?: string }) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...userUpdate };
+      }
+      return user;
+    });
+    return this.findById(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findById(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
+}
